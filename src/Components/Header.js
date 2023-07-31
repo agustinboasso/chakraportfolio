4e8ae04c2bdebc53a6aa0fbf48d916ec491700a5
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-07-31 16:58:31 -0300

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -23,14 +23,14 @@ function Header() {
         alignSelf="flex-end"
       />
       <Flex
-        direction={isNotSmallerScreen ? 'row' : 'column'}
+      justifyContent="center" alignItems="center" flexDirection={{ base: "column", md: "row" }}
         spacing="200px"
         p={isNotSmallerScreen ? '32' : '0'}
         alignSelf="flex-start"
       >
-        <Box mt={isNotSmallerScreen ? '0' : 16} align="flex-start">
+        <Box mt={isNotSmallerScreen ? '0' : 16} align="flex-start" mr={isNotSmallerScreen ? "1rem" : "5rem"}>
           <Text fontSize="5xl" fontWeight="semibold">
-            Hola!, <br />soy
+            Hola! Soy
           </Text>
           <Text
             fontSize="7xl"
@@ -38,10 +38,10 @@ function Header() {
             bgGradient="linear(to-r, cyan.400, blue.500, purple.600)"
             bgClip="text"
           >
-            Agustin Boasso
+            Agustin <br/>Boasso
           </Text>
           <Text color={isDark ? 'gray.200' : 'gray.500'}>
-            Desarrollador Fullstack de la ciudad de Córdoba, Argentina. 🗣
+            Desarrollador Fullstack <br/> Córdoba, Argentina. 🗣
           </Text>
           <IconButton
             mt={8}
