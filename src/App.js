74c30a73f74bca86443475e8bdbf0120c6f52fd1
Agustin Boasso <boassoagustin@gmail.com>
2023-07-31 14:12:41 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { IconButton } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { Flex, VStack, Heading, Spacer } from "@chakra-ui/layout";
+import { FaSun, FaMoon, FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa'
+import Header from "./Components/Header";
+import Profile from "./Components/Profile";
+import Social from "./Components/Social";
 
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+
+  // URLs de tus redes sociales
+  const linkedinURL = "https://www.linkedin.com/in/agustin-boasso-/";
+  const instagramURL = "https://www.instagram.com/boasso_agustin/?hl=en";
+  const githubURL = "http://github.com/agustinboasso";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack p={5}>
+      <Flex w="100%">
+        <Heading ml="8" size="md" fontWeight='semibold' color="cyan.400">Agustin Boasso</Heading>
+        <Spacer></Spacer>
+        <IconButton as="a" href={linkedinURL} target="_blank" rel="noopener noreferrer" icon={<FaLinkedin />} isRound='true' />
+        <IconButton as="a" href={instagramURL} target="_blank" rel="noopener noreferrer" icon={<FaInstagram />} isRound='true' />
+        <IconButton as="a" href={githubURL} target="_blank" rel="noopener noreferrer" icon={<FaGithub />} isRound='true' />
+        <IconButton ml={8} icon={isDark ? <FaSun /> : <FaMoon />} isRound='true' onClick={toggleColorMode} />
+      </Flex>
+      <Header />
+      <Social />
+      <Profile />
+    </VStack>
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,42 @@
+import { Button } from '@chakra-ui/button';
+import { useColorMode } from '@chakra-ui/color-mode'
+import { Image } from '@chakra-ui/image';
+import { Stack, Circle, Flex, Box, Text } from '@chakra-ui/layout';
+import { useMediaQuery } from '@chakra-ui/media-query';
+import React from 'react'
+
+function Header() {
+
+    const { colorMode } = useColorMode();
+    const isDark = colorMode === "dark";
+
+    const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
+
+    return (
+        <Stack>
+            <Circle position="absolute" bg="blue.100" opacity="0.1"
+                w="300px" h="300px" alignSelf="flex-end" />
+            <Flex direction={isNotSmallerScreen ? "row" : "column"}
+                spacing="200px" p={isNotSmallerScreen ? "32" : "0"}
+                alignSelf="flex-start">
+                <Box mt={isNotSmallerScreen ? "0" : 16} align='flex-start'>
+                    <Text fontSize="5xl" fontWeight="semibold">Hola!, soy</Text>
+                    <Text fontSize="7xl" fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, purple.600)" bgClip='text' >Agustin Boasso</Text>
+                    <Text color={isDark ? "gray.200" : "gray.500"}>Desarrollador Fullstack de la ciudad de Córdoba, Argentina.
+              🗣</Text>
+                    <Button mt={8} colorScheme="blue" onClick={() =>
+                        window.open("https://pawan.live")
+                    }>Contáctame</Button>
+
+                </Box>
+                <Image alignSelf="center" mt={isNotSmallerScreen ? "0" : "12"}
+                    mb={isNotSmallerScreen ? "0" : "12"} borderRadius='full'
+                    backgroundColor="transparent" boxShadow="lg"
+                    boxSize="300px" src='https://media.licdn.com/dms/image/D4D03AQEO8Bj8Ae5HDQ/profile-displayphoto-shrink_800_800/0/1670810727142?e=1696464000&v=beta&t=pw-Pzk4RIN0ytVE9fol-Jf9eYUgTx-CDAZKsT6mkEzM' />
+            </Flex>
+
+        </Stack>
+    )
+}
+
+export default Header
