4b0d292379aa04ca98b5f8684408b1b6e315e3b7
Agustin Boasso <boassoagustin@gmail.com>
2023-09-20 12:05:22 -0300

    update cv

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -15,7 +15,7 @@ function Header() {
 
   const handleDownloadCV = () => {
     window.open(
-      'https://www.mediafire.com/file/swbce6zgvs9q5ih/AGUSTIN_BOASSO_CV.pdf/file',
+      'https://www.mediafire.com/file/1mxlj5c4w00f1wb/AGUSTIN_BOASSO_CV.pdf/file',
       '_blank'
     );
   };
